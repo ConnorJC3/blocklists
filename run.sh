ab4c5f0c9d1d25760671036f01365a066f7f47a4
ConnorJC <ccatlett2000@mctherealm.net>
2020-12-04 20:08:41 -0500

    New TV attempt

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p output
+rm -f output/*.txt
 while read list; do
   curl $list | sed '/^[[:blank:]]*#/d;/^$/d;s/#.*//;s/^/0.0.0.0 /' > output/$(basename $list)
 done < lists.txt
