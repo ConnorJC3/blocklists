aa0103aa6befbf3b19e9f734b884f4a20e6dfa9e
ConnorJC <ccatlett2000@mctherealm.net>
2020-12-04 20:35:53 -0500

    Back to known working list

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 mkdir -p output
 rm -f output/*.txt
 while read list; do
-  curl $list | sed '/^[[:blank:]]*#/d;/^$/d;s/#.*//;s/^/0.0.0.0 /' > output/$(basename $list)
+  curl $list \
+  | sed '/^[[:blank:]]*#/d;/^$/d;s/#.*//;s/^/0.0.0.0 /' \
+  | sed '/time\.samsungcloudsolution\.com/d' \
+  > output/$(basename $list)
 done < lists.txt
