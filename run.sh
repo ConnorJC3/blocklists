04dcabe1221dc80f6b947a5a77cf945c850f4d6b
ConnorJC <ccatlett2000@mctherealm.net>
2020-12-04 22:18:19 -0500

    Re-whitelist

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ while read list; do
   curl $list \
   | sed '/^[[:blank:]]*#/d;/^$/d;s/#.*//;s/^/0.0.0.0 /' \
   | sed '/time\.samsungcloudsolution\.com/d' \
+  | sed '/cdn\.samsungcloudsolution\.com/d' \
   > output/$(basename $list)
 done < lists.txt
